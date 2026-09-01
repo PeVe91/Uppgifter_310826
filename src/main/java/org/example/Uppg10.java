fd262a381f65d7ee58d47f590fdfa1d7ccac1aad
PeVe91 <per.vestin@iths.se>
2026-09-01 07:51:13 +0200

    1 - 10

--- src/main/java/org/example/Uppg10.java
+++ src/main/java/org/example/Uppg10.java
@@ -3,5 +3,12 @@ package org.example;
 public class Uppg10 {
     static void main() {
 
+        int inputCelcius = Integer.parseInt(IO.readln("Celcius to Farenheit converter: "));
+
+        System.out.println(temperatureConverter(inputCelcius));
+    }
+    static double temperatureConverter (int celcius) {
+        double farenheit = celcius * 1.8 + 32;
+        return farenheit;
     }
 }
--- src/main/java/org/example/Uppg9.java
+++ src/main/java/org/example/Uppg9.java
@@ -3,5 +3,14 @@ package org.example;
 public class Uppg9 {
     static void main() {
 
+        int add = sum(3, 6);
+
+        System.out.println(add);
+
     }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
 }
