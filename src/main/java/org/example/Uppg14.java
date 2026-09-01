fbbbb7fbea7b1b33fb53abbbf22fe2d5f7f8cb61
PeVe91 <per.vestin@iths.se>
2026-09-01 09:54:02 +0200

    Tillagt uppgift 14

---
+++ src/main/java/org/example/Uppg14.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Uppg14 {
+    static void main() {
+
+        int inputNumber = Integer.parseInt(IO.readln("Enter a number: "));
+
+        System.out.println(fizzBuzz(inputNumber));
+
+    }
+
+    static String fizzBuzz(int number) {
+        if (number % 5 == 0 && number % 3 == 0) {
+            return "FizzBuzz";
+        }  else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "";
+        }
+    }
+}
